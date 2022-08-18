9ebede31bef2ae0708ed8cd1b8ae6425146b204c
Vikrant <vikrant.vishwakarma07@gmail.com>
2022-08-18 15:13:30 +0530

    Adding strict slashes to false to allow URI to end with slash as optional

--- demo/demo.py
+++ demo/demo.py
@@ -18,6 +18,7 @@ except KeyError as e:
 if __name__ == "__main__":
     from views import *
     app.run()
+    app.url_map.strict_slashes = False
 """
 curl --location --request POST 'http://127.0.0.1:5000/register/' --header 'Content-Type: application/json' --data-raw '{ "first_name": "Vikrant", "last_name": "Vishwakarma", "email": "vikrant.vishwakarma07@gmail.com", "password": "9591297013" }'
 """
